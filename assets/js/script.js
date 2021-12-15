7160b022b5fe7cb951acf898d0fbcfbfcd6c96bc
Iain <iain@polevaultweb.com>
2021-12-15 09:12:24 +0000

    Subnav should be toggled on URL hash.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,28 @@
-jQuery(function($) {
-    const container = $('#wpdb');
-    const nav = container.find('.subnav');
+(function( $ ) {
+	const container = $( '#wpdb' );
+	const nav = container.find( '.subnav' );
 
-    nav.on( 'click', 'a', function (e) {
-        e.preventDefault();
+	function toggle_nav( type ) {
+		$( '#wpdb .subnav a' ).removeClass( 'active' );
+		$( '#wpdb .subnav a[data-type="' + type + '"]' ).addClass( 'active' );
 
-        nav.find('a').removeClass('active');
-        $(this).addClass('active');
+		const visibleClass = type + '-content';
+		container.find( 'fieldset' ).hide();
+		container.find( '.' + visibleClass ).show();
 
-        const visibleClass = $(this).attr('href').substr(1) + '-content';
-        container.find('fieldset').hide();
-        container.find('.' + visibleClass).show();
-    });
-});
+		window.location.hash = type;
+	}
+
+	nav.on( 'click', 'a', function( e ) {
+		e.preventDefault();
+		toggle_nav( $( this ).attr( 'href' ).substr( 1 ) );
+	} );
+
+	$( document ).on( 'ready', function() {
+		var type = window.location.hash.substr( 1 );
+		if ( type.length && ['backup', 'schedule'].includes( type ) ) {
+			toggle_nav( type );
+		}
+	} );
+
+})( jQuery );
